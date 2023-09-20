3132f494559f94b30a23ea9b23db4e2db8c8dae1
kejbeg <kejbegofficial@gmail.com>
2023-09-20 18:17:31 +0200

    initial setup

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,29 @@
 const express = require('express');
 const router = express.Router();
 
+// Import database
+const dbConnection = require(`../database`);
+const connection = require('../database');
+
+// Routes
 router.get('/', (req, res) => {
 	res.render('index');
+});
+
+router.post('/', (req, res) => {
+	let message = req.body.message;
+	connection.query(
+		`INSERT INTO messages (message) VALUES ('${message}')`,
+		(error, result) => {
+			if (error) {
+				console.log(`An error occured while inserting the message ${error}`);
+			} else {
+				console.log('Message inserted successfully');
+			}
+		}
+	);
+
+	res.redirect('/');
 });
 
 // Export the router
