c11a11934fe73e6e57ad9462da39edcd825ef536
Hat Dealer <jan.richter2007@gmail.com>
2023-09-20 20:05:30 +0200

    did almost nothing lika a dumbass

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 // Import database
-const dbConnection = require(`../database`);
 const connection = require('../database');
 
 // Routes
