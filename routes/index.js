399bd773c1ffff63309c34e967b52dbd5b79fd90
kejbeg <kejbegofficial@gmail.com>
2023-09-20 20:04:25 +0200

    SQL messages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 // Import database
-const dbConnection = require(`../database`);
 const connection = require('../database');
 
 // Routes
@@ -13,8 +12,10 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
 	let message = req.body.message;
+	let author = 'Anonymous';
 	connection.query(
-		`INSERT INTO messages (message) VALUES ('${message}')`,
+		`INSERT INTO messages (message, author) VALUES (?,?)`,
+		[message, author],
 		(error, result) => {
 			if (error) {
 				console.log(`An error occured while inserting the message ${error}`);
