bd352bdc0e8957ebc4d8282bd3ad033941168c6b
kejbeg <kejbegofficial@gmail.com>
2023-10-24 21:15:07 +0200

    test

--- database.js
+++ database.js
@@ -4,6 +4,7 @@ const mysql = require('mysql');
 // Database connection
 const connection = mysql.createConnection({
 	host: process.env.DATABASE_HOST,
+	port: process.env.DATABASE_PORT,
 	user: process.env.DATABASE_USER,
 	password: process.env.DATABASE_PASSWORD,
 	database: process.env.DATABASE_NAME,
