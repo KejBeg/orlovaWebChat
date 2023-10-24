329e8f08333750d89b43bc52ba5195090eda8194
kejbeg <kejbegofficial@gmail.com>
2023-10-25 01:30:22 +0200

    test

--- public/js/socket.js
+++ public/js/socket.js
@@ -2,7 +2,7 @@
 const currentUrl = window.location.href;
 
 // Get the domain name, remove the port, change protocol to ws
-const socketIoUrl = 'wss://' + currentUrl.split('/')[2].split(':')[0];
+const socketIoUrl = currentUrl.split('/')[2].split(':')[0];
 
 const socket = io({
 	secure: true,
