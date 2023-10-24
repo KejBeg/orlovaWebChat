132c488b2f75fb70d40cc5426989041a1a5f4530
kejbeg <kejbegofficial@gmail.com>
2023-10-24 20:23:43 +0200

    Removed useless messages

--- createMessages.sh
+++
@@ -1,5 +0,0 @@
-# Script just to create story messages for testing
-# for i in {1..10}
-# do
-# done
-mysql -u root -ptest1234 orlovaWebApp -e "INSERT INTO storyMessages (id, answer, question, answer1Id, answer2id, answer3Id) VALUES (0, 'test', 'test', 0, 0, 0);"
