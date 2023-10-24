89e6d6ba175f9ae6d856e9f7432593ea8917426c
kejbeg <kejbegofficial@gmail.com>
2023-10-25 01:33:55 +0200

    test

--- public/js/socket.js
+++ public/js/socket.js
@@ -4,12 +4,7 @@ const currentUrl = window.location.href;
 // Get the domain name, remove the port, change protocol to ws
 const socketIoUrl = currentUrl.split('/')[2].split(':')[0];
 
-const socket = io({
-	secure: true,
-	transports: ['websocket'],
-	upgrade: false,
-	path: '/socket.io',
-});
+const socket = io.connect();
 
 // Creating offensive message text
 const offensiveMessageText = 'This message has been flagged as offensive'
